811d9a13858d98f1961ec987d16df60082dcecaf
Raul-INSBC <raul.garrido@insbaixcamp.cat>
2020-12-18 14:26:30 +0100

    Activitat7-M3-UF5

--- src/M3/UF5/NF3/Activitat7/EmptySudokuTester.java
+++ src/M3/UF5/NF3/Activitat7/EmptySudokuTester.java
@@ -1,52 +1,67 @@
 package M3.UF5.NF3.Activitat7;
 
+import java.util.Scanner;
+
 public class EmptySudokuTester {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int cantidad = teclado.nextInt();
 
-        char sudoku[][] = { {'2','-','-','5','-','9','2','-','7'},
-                            {'6','7','-','3','-','-','-','-','4'},
-                            {'-','-','-','-','-','2','-','-','-'},
-                            {'-','4','9','2','-','-','-','8','5'},
-                            {'-','-','-','-','-','-','-','-','-'},
-                            {'2','3','-','-','-','4','1','6','-'},
-                            {'-','-','-','1','-','-','-','-','-'},
-                            {'8','-','-','-','-','5','-','9','1'},
-                            {'8','-','4','9','-','7','-','-','6'}};
+        for (int i = 0; i < cantidad; i++) {
+            char sudoku[][] = new char[9][9];
+            String valor;
+            valor = teclado.next();
+            int caracter = 0;
 
-        int contadorNums = 0;
-
-        boolean sudokuCorrecto = true;
-
-        int x = 8;
-        int y = 8;
-
-        for (int i = 0; i < sudoku.length; i++) {
             for (int j = 0; j < sudoku.length; j++) {
-
-                System.out.print(sudoku[i][j]);
-
-                if (sudoku[i][j] != '-') {
-                    contadorNums++;
-
+                for (int k = 0; k < sudoku.length; k++) {
+                    sudoku[j][k] = valor.charAt(caracter);
+                    caracter++;
                 }
-
-                if ((sudoku[i][j] != '-' && sudoku[i][j] != '-') || (sudoku[x][y] == '-' && sudoku[x][y] == '-')) {
-
-                } else {
-                    sudokuCorrecto = false;
-
-                }
-                y--;
             }
-            System.out.println();
-            y=8;
-            x--;
-        }
 
-        if (sudokuCorrecto && contadorNums <33) {
-            System.out.println("SI");
-        } else {
-            System.out.println("NO");
+            int contadorNums = 0;
+
+            boolean sudokuCorrecto = true;
+
+            int x = sudoku.length;
+            int y = sudoku.length;
+
+            for (int j = 0; j < sudoku.length; j++) {
+                for (int k = 0; k < sudoku.length; k++) {
+
+                    // Si hay un numero en la casilla y en su casilla opuesta suma 2 numeros al contador y se mantiene
+                    // en correcto
+                    if ((sudoku[j][k] != '-' && sudoku[j][k] != '-')) {
+                        contadorNums += 2;
+
+                        // Si no hay un numero en la casilla ni en su casilla opuesta se mantiene en correcto
+                    } else if ((sudoku[x][y] == '-' && sudoku[x][y] == '-')) {
+
+                        // Si el valor de la casilla y su casilla opuesta son diferentes, marca el sudoku como
+                        // incorrecto y termina los bucles for
+                    } else {
+                        sudokuCorrecto = false;
+                        j = sudoku.length;
+                        k = sudoku.length;
+                    }
+                    y--;
+                }
+                y=8;
+                x--;
+            }
+
+            if (sudokuCorrecto && contadorNums <33) {
+                System.out.println("SI");
+            } else {
+                System.out.println("NO");
+            }
         }
     }
 }
+// ---5-92-767-3----4-----2----492---85---------23---416----1-----5----8-918-49-7--- (SI)
+// ---5-92-767-3----4-----2----492---85---------23---416----------5----8-918-49-7--- (NO)
+// 2--5-92-767-3----4-----2----492---85---------23---416----1-----8----5-918-49-7--6 (SI)
+// -----92-767-3----4-----2----499---85-3-----2-28---416----4-----5----8-918-49----- (SI)
+// 1-------2---------------------------------------------------------------3-------4 (SI)
+// 123456789234567891345678912456789123567891234678912345789123456891234567987654321 (NO)
