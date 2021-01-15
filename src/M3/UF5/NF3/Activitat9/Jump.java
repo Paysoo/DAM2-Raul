dd38fa54dbce3053f1d9b4b301b7bbda0b413fe0
Raul-INSBC <raul.garrido@insbaixcamp.cat>
2021-01-15 17:10:14 +0100

    Activitat 9 JumpTester M3

--- src/M3/UF5/NF3/Activitat9/Jump.java
+++ src/M3/UF5/NF3/Activitat9/Jump.java
@@ -10,35 +10,24 @@ public class Jump {
         double[] notas = new double[7];
 
         for (int i = 0; i < notas.length; i++) {
-            notas[i] = teclado.nextDouble();
+        notas[i] = teclado.nextDouble();
         }
 
 //        JumpTester jumpTester = new JumpTester();
 
-        double numeroMayor = -0.5;
-        double numeroMenor = 10.5;
-        for (int i = 0; i < notas.length; i++) {
-            if(notas[i]>numeroMayor) {
-                numeroMayor = notas[i];
-            }
-            if (notas[i]<numeroMenor){
-                numeroMenor = notas[i];
-            }
-        }
-
         Arrays.sort(notas);
         double suma = 0.0;
         for (int i = 0; i < notas.length; i++) {
-            if (i != 0 && i != 1 && i != 5 && i!=6) {
-                suma += notas[i];
-            }
+        if (i != 0 && i != 1 && i != 5 && i!=6) {
+        suma += notas[i];
+        }
         }
         double notaFinal = suma * 2;
 
         System.out.println(suma);
 
         for (int i = 0; i < notas.length; i++) {
-            System.out.println(notas[i]);
+        System.out.println(notas[i]);
         }
         System.out.println(notaFinal);
 
@@ -48,8 +37,8 @@ public class Jump {
 //            System.out.println(notas[i]);
 //        }
 
-    }
-}
+        }
+        }
 /* 4,4
 9,9
 6,6
