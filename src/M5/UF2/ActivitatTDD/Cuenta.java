bfa60001085533681eb6f392750d84bfa3f9adae
Raul-INSBC <raul.garrido@insbaixcamp.cat>
2021-03-10 20:10:02 +0100

    AEA-M5-UF2 TDD (Avaluable NO DUAL)

--- src/M5/UF2/ActivitatTDD/Cuenta.java
+++ src/M5/UF2/ActivitatTDD/Cuenta.java
@@ -1,8 +1,17 @@
 package M5.UF2.ActivitatTDD;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class Cuenta {
 
     private double saldo;
+    static final int INGRESO_MAXIMO = 6000;
+    static final int RETIRADA_MAXIMA = 6000;
+    static final int TRANSFERENCIA_MAXIMA = 3000;
+    private double tranferenciaHecha;
+    private String ultimaTransferencia = "0000";
 
     public Cuenta() {
     }
@@ -13,8 +22,35 @@ public class Cuenta {
     }
 
     public void ingresar(double ingreso) {
-        if (ingreso > 0 && tieneDosDecimales_o_Menos(ingreso)) {
+        if (ingreso > 0 && tieneDosDecimales_o_Menos(ingreso) && ingreso <= INGRESO_MAXIMO) {
             this.saldo += ingreso;
+        }
+    }
+
+    public void retirar(double retiro) {
+        if (this.saldo >= retiro && retiro > 0 && tieneDosDecimales_o_Menos(retiro) && retiro <= RETIRADA_MAXIMA) {
+            this.saldo -= retiro;
+        }
+    }
+
+    public void transferir(double transferencia, Cuenta receptor) {
+
+        if (transferencia > 0 && transferencia <= TRANSFERENCIA_MAXIMA) {
+            if (this.ultimaTransferencia.equals(fecha())) {
+                if (this.tranferenciaHecha + transferencia <= TRANSFERENCIA_MAXIMA) {
+                    receptor.ingresar(transferencia);
+                    this.retirar(transferencia);
+                    this.tranferenciaHecha += transferencia;
+                    this.ultimaTransferencia = fecha();
+                }
+            } else {
+                this.tranferenciaHecha = 0;
+                receptor.ingresar(transferencia);
+                this.retirar(transferencia);
+                this.tranferenciaHecha += transferencia;
+                this.ultimaTransferencia = fecha();
+            }
+
         }
     }
 
@@ -22,5 +58,19 @@ public class Cuenta {
 
         return ingreso == Math.floor(ingreso * 100) / 100;
 
+    }
+
+    private String fecha(){
+        String data = "";
+
+        Calendar c = new GregorianCalendar();
+
+        String dia = Integer.toString(c.get(Calendar.DATE));
+        String mes = Integer.toString(c.get(Calendar.MONTH));
+        String annio = Integer.toString(c.get(Calendar.YEAR));
+
+        data = dia+mes+annio;
+
+        return data;
     }
 }
