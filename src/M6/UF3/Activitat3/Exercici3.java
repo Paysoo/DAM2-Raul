eec7630cee9ff15cd01ff7ba8b3e4ac35ed5fe89
= <=>
2021-03-19 19:21:19 +0100

    M3 Recursivitat Activitat1

--- src/M6/UF3/Activitat3/Exercici3.java
+++ src/M6/UF3/Activitat3/Exercici3.java
@@ -4,7 +4,12 @@ import org.xmldb.api.*;
 import org.xmldb.api.base.*;
 import org.xmldb.api.modules.*;
 
+import java.sql.ResultSet;
+import java.util.Scanner;
+
 public class Exercici3 {
+    static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) throws XMLDBException {
         //Driver per a eXist
         String driver = "org.exist.xmldb.DatabaseImpl";
@@ -33,23 +38,127 @@ public class Exercici3 {
         if (col == null)
             System.out.println("*** LA COL·LECCIÓ NO EXISTEIX ***");
 
+        int opcion;
+        boolean salir = false;
         XPathQueryService servei = (XPathQueryService) col.getService("XPathQueryService", "1.0");
 
-        ResourceSet result = servei.query("for $em in /EMPLEADOS/EMP_ROW[DEPT_NO=20] return $em");
+        while (!salir) {
+            mostrardeps(servei);
+            System.out.println("Introduce una opcion entre 1 y 4]" +
+                    "\n1-Introducir departamento" +
+                    "\n2-Borrar departamento" +
+                    "\n3-Modificar departamento" +
+                    "\n4-Salir");
+            opcion = teclado.nextInt();
 
-
-        //Recorrer les dades del recurs
-        ResourceIterator i;
-        i = result.getIterator();
-
-        if (!i.hasMoreResources())
-            System.out.println("LA CONSULTA NO TORNA RES");
-        while (i.hasMoreResources()) {
-            Resource r = i.nextResource();
-            System.out.println((String)r.getContent());
+            switch (opcion) {
+                case 1:
+                    insereixdep(servei);
+                    break;
+                case 2:
+                    esborradep(servei);
+                    break;
+                case 3:
+                    modificadep(servei);
+                    break;
+                case 4:
+                    salir = true;
+                default:
+                    break;
+            }
         }
+
         //S'esborra
         col.close();
 
     }
+
+    public static void insereixdep(XPathQueryService servei) throws XMLDBException {
+        ResourceSet result;
+        int depNum;
+        String depNom;
+        String depLocalitat;
+
+        System.out.println("Introduce el num de departamento");
+        depNum = teclado.nextInt();
+        System.out.println("Introduce el nombre del departamento");
+        depNom = teclado.next();
+        System.out.println("Introduce la localidad del departamento");
+        depLocalitat = teclado.next();
+
+        result = servei.query("for $dep in /departamentos/DEP_ROW[DEPT_NO=" + depNum + "] return $dep");
+        ResourceIterator i;
+        i = result.getIterator();
+
+        if (!i.hasMoreResources()) {
+            servei.query("update insert <DEP_ROW><DEPT_NO>"
+                    + depNum + "</DEPT_NO><DNOMBRE>" + depNom + "</DNOMBRE><LOC>" + depLocalitat
+                    + "</LOC></DEP_ROW> into /departamentos");
+        } else {
+            System.out.println("Ya existe un departamento con ese numero");
+            System.out.println();
+        }
+
+    }
+
+    public static void esborradep(XPathQueryService servei) throws XMLDBException {
+        ResourceSet result;
+        int depNum = 0;
+
+        System.out.println("Escribe el numero del DEPARTAMENTO que quieres ELIMINAR ");
+        depNum = teclado.nextInt();
+
+        result = servei.query("for $dep in /departamentos/DEP_ROW[DEPT_NO=" + depNum + "] return $dep");
+        ResourceIterator i;
+        i = result.getIterator();
+
+        if (!i.hasMoreResources()) {
+            System.out.println("No hay departamentos con ese numero");
+            System.out.println();
+        } else {
+            servei.query("update delete /departamentos/DEP_ROW[DEPT_NO=" + depNum + "]");
+        }
+    }
+
+    public static void modificadep(XPathQueryService servei) throws XMLDBException {
+        ResourceSet result;
+        int depNum;
+        String depNom;
+        String depLocalitat;
+
+        System.out.println("Introduce el num de DEPARTAMENTO que quieres MODIFICAR");
+        depNum = teclado.nextInt();
+        System.out.println("Introduce el nombre del departamento");
+        depNom = teclado.next();
+        System.out.println("Introduce la localidad del departamento");
+        depLocalitat = teclado.next();
+
+        result = servei.query("for $dep in /departamentos/DEP_ROW[DEPT_NO=" + depNum + "] return $dep");
+        ResourceIterator i;
+        i = result.getIterator();
+
+        if (!i.hasMoreResources()) {
+            System.out.println("No hay departamentos con ese numero");
+            System.out.println();
+        } else {
+            servei.query("update replace /departamentos/DEP_ROW[DEPT_NO = " + depNum + "]/DNOMBRE with " +
+                    "<DNOMBRE>" + depNom + "</DNOMBRE>");
+            servei.query("update replace /departamentos/DEP_ROW[DEPT_NO =" + depNum + "]/LOC with " +
+                    "<LOC>" + depLocalitat + "</LOC>");
+        }
+    }
+
+    public static void mostrardeps(XPathQueryService servei) throws XMLDBException {
+        ResourceSet result = servei.query("for $dep in /departamentos return $dep");
+        ResourceIterator i;
+        i = result.getIterator();
+        if (!i.hasMoreResources()) {
+            System.out.println("NO HAY DEPARTAMENTOS");
+        }
+        while (i.hasMoreResources()) {
+            Resource r = i.nextResource();
+            System.out.println((String) r.getContent());
+        }
+    }
+
 }
